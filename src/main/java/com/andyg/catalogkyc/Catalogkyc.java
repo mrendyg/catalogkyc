29f8a952935ccd323edebdddf67718c19592626e
mrendyg <andyg.oyarzo@gmail.com>
2024-01-31 16:33:11 -0300

    avances parte grafica

---
+++ src/main/java/com/andyg/catalogkyc/Catalogkyc.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.andyg.catalogkyc;
+
+import com.andyg.catalogkyc.gi.menuseleccionmodelo;
+import javax.swing.JFrame;
+
+
+public class Catalogkyc {
+
+    public static void main(String[] args) {
+        JFrame menu = new JFrame("Catalogo KYC");
+        
+        menuseleccionmodelo menuseleccion = new menuseleccionmodelo();
+        
+        menu.add(menuseleccion);
+        
+        menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        menu.pack();
+        menu.setVisible(true);
+        menu.setLocationRelativeTo(null);
+           
+    }
+}
